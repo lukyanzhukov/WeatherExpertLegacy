9564329e24d7d3e37e634f41a2eafc48945594c0
Lukyan Zhukov <lukyanzhukov@yandex.ru>
2021-04-19 23:30:41 +0700

    WeatherExpertDatabase: share database module for features.

--- feature/city/src/main/java/com/lukianbat/feature/city/data/local/gateway/CitiesLocalGateway.kt
+++ feature/city/src/main/java/com/lukianbat/feature/city/data/local/gateway/CitiesLocalGateway.kt
@@ -2,14 +2,14 @@ package com.lukianbat.feature.city.data.local.gateway
 
 import com.lukianbat.feature.city.data.local.mapper.DBMapper.toDb
 import com.lukianbat.feature.city.data.local.mapper.DBMapper.toDomain
-import com.lukianbat.feature.city.db.CityDao
+import com.lukianbat.weatherexpertdb.WeatherExpertDao
 import com.lukianbat.feature.city.domain.model.CityModel
 import io.reactivex.Completable
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
-class CitiesLocalGateway @Inject constructor(private val dao: CityDao) {
+class CitiesLocalGateway @Inject constructor(private val dao: WeatherExpertDao) {
 
     fun getCitiesList(): Single<List<CityModel>> = dao
         .getCities()
